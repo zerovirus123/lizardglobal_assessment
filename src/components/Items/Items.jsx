a834228ce6883dbeee260b285a34d2047d3453c9
zerovirus123 <briansia93@gmail.com>
2022-10-23 23:55:01 +0700

    REFACTOR: refactor conditional render to remove duplication

--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -2,15 +2,7 @@ import React from 'react'
 import moment from 'moment/moment'
 import "../Items/Items.css"
 
-const Items = ({currentItems, selectedOptions}) => {
-  let selectedOptionsValue = []
-  selectedOptions.map((option) => {
-    selectedOptionsValue.push(option.value)
-  })
-
-  const foundCategory = (item) => {
-    return item.categories.some((category) => selectedOptionsValue.includes(category.name))
-  }
+const Item = ({item}) => {
 
   const parseDateString = (date) => {
     return moment(date).utc().format('YYYY-MM-DD hh:mm:ss A')
@@ -22,48 +14,49 @@ const Items = ({currentItems, selectedOptions}) => {
 
   return (
     <>
+      <div className="item-container">
+        <img src={splitURL(item.author.avatar)} alt={""} />
+        <li className="list-content">
+          <div className="data-header">Title</div>
+          <div className="data-content">{item.title}</div>
+        </li>
+        <li className="list-content">
+          <div className="data-header">Author</div>
+          <div className="data-content">{item.author.name}</div>
+        </li>
+        <li className="list-content">
+          <div className="data-header">Published Date</div>
+          <div className="data-content">{parseDateString(item.publishDate)}</div>
+        </li>
+        <li className="list-content">
+          <div className="data-header">Summary</div>
+          <div className="data-content">{item.summary}</div>
+        </li>
+      </div>
+    </>
+  )
+}
+
+const Items = ({currentItems, selectedOptions}) => {
+  let selectedOptionsValue = []
+  selectedOptions.map((option) => {
+    selectedOptionsValue.push(option.value)
+  })
+
+  const foundCategory = (item) => {
+    return item.categories.some((category) => selectedOptionsValue.includes(category.name))
+  }
+
+  // if no categories are selected, render everything
+  // else only render items that fall under the selected category
+  return (
+    <>
       <ul className="list">
       {currentItems && currentItems.map((item, key) => (
           <>
             {selectedOptions.length === 0 ?
-              <div className="item-container">
-                <img src={splitURL(item.author.avatar)} alt={""} />
-                <li className="list-content">
-                  <div className="data-header">Title</div>
-                  <div className="data-content">{item.title}</div>
-                </li>
-                <li className="list-content">
-                  <div className="data-header">Author</div>
-                  <div className="data-content">{item.author.name}</div>
-                </li>
-                <li className="list-content">
-                  <div className="data-header">Published Date</div>
-                  <div className="data-content">{parseDateString(item.publishDate)}</div>
-                </li>
-                <li className="list-content">
-                  <div className="data-header">Summary</div>
-                  <div className="data-content">{item.summary}</div>
-                </li>
-              </div> : foundCategory(item) ?
-              <div className="item-container">
-                <img src={splitURL(item.author.avatar)} alt={""} />
-                <li className="list-content">
-                  <div className="data-header">Title</div>
-                  <div className="data-content">{item.title}</div>
-                </li>
-                <li className="list-content">
-                  <div className="data-header">Author</div>
-                  <div className="data-content">{item.author.name}</div>
-                </li>
-                <li className="list-content">
-                  <div className="data-header">Published Date</div>
-                  <div className="data-content">{parseDateString(item.publishDate)}</div>
-                </li>
-                <li className="list-content">
-                  <div className="data-header">Summary</div>
-                  <div className="data-content">{item.summary}</div>
-                </li>
-              </div> : null
+              <Item item={item}/> : foundCategory(item) ?
+              <Item item={item} /> : null
             }
           </>
       ))}
